b297331fed554ecac40e98cfc8a17b265541ad33
EddWills95 <edd.williams@me.com>
2021-12-21 17:56:24 +0000

    update date format for easier hashing

--- logger.js
+++ logger.js
@@ -1,15 +1,16 @@
 var fs = require("fs");
+var os = require("os");
 var log_file = fs.createWriteStream(process.env.LOG_LOCATION, { flags: "a" });
 
 const generateTimeStamp = () => {
   const date = new Date();
-  return `${date.toLocaleDateString()}::${date.toLocaleTimeString()} `;
+  return `${date.toISOString()} `;
 };
 
 const Logger = {
   log: (data) => {
     console.log(data);
-    log_file.write(generateTimeStamp() + data + "\n");
+    log_file.write(generateTimeStamp() + data + os.EOL);
   },
   writeOut: () => {
     log_file.writeOut();
